1b0438c260e37523d0e56ffe55d46cb98cc4ecb2
Khalyomede <khalyomede@gmail.com>
2020-10-21 23:21:50 +0200

    fixed wrong navbar links

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="table mt-4 mb-10 w-full">
-	<a href="/" class="font-bold hover:underline table-cell">{{ $brand }}</a>
+	<a href="{{ $page->baseUrl }}/" class="font-bold hover:underline table-cell">{{ $brand }}</a>
 	<div class="table-cell text-right">
-		<a href="/about/" class="hover:underline">About</a>
+		<a href="{{ $page->baseUrl }}/about/" class="hover:underline">About</a>
 	</div>
 </nav>
